1ca8aee4bcd738120d1d9c1822af3311ba056a92
Cheng Kang (康成) <42560067+ChengKang520@users.noreply.github.com>
2022-03-30 16:26:53 +0200

    Update t5_symtoms_extract_binary.py

--- t5/t5_symtoms_extract_binary.py
+++ t5/t5_symtoms_extract_binary.py
@@ -4,27 +4,9 @@
 # For example, running this (by clicking run or pressing Shift+Enter) will list all files under the input directory
 
 import os
-
-# You can write up to 5GB to the current directory (/kaggle/working/) that gets preserved as output when you create a version using "Save & Run All"
-# You can also write temporary files to /kaggle/temp/, but they won't be saved outside of the current session
-
-# Import libraries
-# This Python 3 environment comes with many helpful analytics libraries installed
-# It is defined by the kaggle/python Docker image: https://github.com/kaggle/docker-python
-# For example, here's several helpful packages to load
-# https://www.kaggle.com/parthplc/t5-fine-tuning-tutorial#T5-text-to-text-transformer
-
-
 import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 from tqdm import tqdm
-# Input data files are available in the read-only "../input/" directory
-# For example, running this (by clicking run or pressing Shift+Enter) will list all files under the input directory
-
-# You can write up to 5GB to the current directory (/kaggle/working/) that gets preserved as output when you create a version using "Save & Run All"
-# You can also write temporary files to /kaggle/temp/, but they won't be saved outside of the current session
-
-# Import libraries
 import argparse
 import os
 import json
